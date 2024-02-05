a9d9d00b0d55418cb60972fa9c8f1b74fa3742ce
Michael <Michael_oyola@outlook.com>
2024-02-05 15:44:51 -0500

    fixing about us page

---
+++ src/components/AboutUsPage/aboutTheTeam.jsx
@@ -0,0 +1,18 @@
+
+import team from '../../assets/team-example.jpg'
+
+
+export default function TheTeam (){
+
+    return (
+        <>
+            <div>
+                <div>
+                    <h1>Our Team</h1>
+                    <div></div>
+                </div>
+                <img src={team}></img>
+            </div>
+        </>
+    )
+}
--- src/components/AboutUsPage/aboutUsPage.jsx
+++ src/components/AboutUsPage/aboutUsPage.jsx
@@ -1,6 +1,7 @@
 
 import sunset from '../../assets/sunset.jpg'
 import GeneralServices from '../Services/generalServices'
+import TheTeam from './aboutTheTeam'
 import './aboutUs.css'
 
 
@@ -14,6 +15,8 @@ export default function AboutUs(){
                     <img className='SunsetImage' src={sunset}></img>
                     <div className='LearmMoreAboutUs'>Learn More About Us</div>
                 </div>
+
+                <TheTeam />
 
                 <GeneralServices />
 
