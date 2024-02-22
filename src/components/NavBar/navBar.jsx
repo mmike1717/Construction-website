3cb5ecfb8b18e4b748c0bde807d30b3e11ac0ecd
Michael <Michael_oyola@outlook.com>
2024-02-21 20:11:11 -0500

    fixed bugs in the homepage

--- src/components/NavBar/navBar.jsx
+++ src/components/NavBar/navBar.jsx
@@ -5,7 +5,7 @@ export default function NavBar(){
 
     return (
         <div id='MainNavBar'>
-            <div id='LogoDiv'>logo</div>
+            <a href='/' id='LogoDiv'>logo</a>
             <div id="NavCenterDiv">
                 <a href='/'>Home</a>
                 <a href='/AboutUs'>About Us</a>
