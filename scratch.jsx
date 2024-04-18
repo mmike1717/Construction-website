28a7245da77204dace838a1ac448e1598a4652f9
Michael <Michael_oyola@outlook.com>
2024-04-18 19:48:09 -0400

    fixing scratch page

--- scratch.jsx
+++ scratch.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
 
 // const Carousel = () => {
 //   const [currentImageIndex, setCurrentImageIndex] = useState(0);
